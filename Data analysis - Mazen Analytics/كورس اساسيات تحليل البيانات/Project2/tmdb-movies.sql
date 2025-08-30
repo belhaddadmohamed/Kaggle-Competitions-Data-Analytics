a464db4acf68de9cac89120be64efc5bc097beb3
belhaddadmohamed <belhaddad.mohamed7@gmail.com>
2025-08-30 02:01:07 +0100

    update outliers

--- Data analysis - Mazen Analytics/كورس اساسيات تحليل البيانات/Project2/tmdb-movies.sql
+++ Data analysis - Mazen Analytics/كورس اساسيات تحليل البيانات/Project2/tmdb-movies.sql
@@ -5,6 +5,26 @@
 -- limit 10
 
 
-select original_title, revenue, budget, `cast`
+-- select original_title, revenue, budget, `cast`
+-- from tmdb_movies
+-- where original_title='The Avengers'
+
+
+-- select concat(substring(original_title, 0, 20), '...'), budget, revenue, genres
+-- from tmdb_movies
+-- where original_title='The Sound of Music' or
+--       original_title='Doctor Zhivago' or
+--       original_title='The Greatest Story Ever Told' or
+--       original_title='Thunderball' or
+--       original_title='Those Magnificent Men in Their Flying Machines or How I Flew from London to Paris in 25 hours 11 minutes';
+
+
+-- select original_title, revenue, budget
+-- from tmdb_movies
+-- where original_title='The Karate Kid, Part II';
+
+
+select original_title, revenue, budget, revenue-budget profit, concat((revenue-budget)/budget, '%') as roi
 from tmdb_movies
-where original_title='The Avengers'
+where original_title='Jurassic Park' or
+      original_title='Minions'
